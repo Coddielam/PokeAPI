2292f73f59e80c16b3d46fb397c2d63cb4e26338
coddielam <edlam711@gmail.com>
2020-06-04 22:55:55 -0400

    search by name working

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-document.querySelector("button").addEventListener("click", function () {
-  const pokemon = document.querySelector("input").value;
+document.querySelector("button").addEventListener("click", function (event) {
+  event.preventDefault();
+  // clear pokemon-viewport
+  document.querySelector("#pokemon-viewport").innerHTML = "";
+  const pokemon = document.querySelector("input").value.toLowerCase();
   getPokemonImage(pokemon);
+  document.querySelector("input").value = "";
 });
 
 function getPokemonImage(pokemon) {
@@ -20,18 +24,56 @@ function getPokemonImage(pokemon) {
   // specifying what to do when the data is loaded
 
   request.onload = function () {
-    var data = JSON.parse(this.response);
+    if (request.status >= 200 && request.status < 400) {
+      var data = JSON.parse(this.response);
+      // target the viewport
+      pokeviewport = document.getElementById("pokemon-viewport");
+      // make an image element and assign the data source
+      pokeImg = document.createElement("img");
+      pokeImg.src = data.sprites.front_default;
+      pokeImg.width = 150;
+      pokeImg.height = 150;
 
-    let div = document.createElement("div");
-    div.setAttribute("class", "pokemon-info");
-    console.log(div);
-    let pokeImg = document.createElement("img");
-    pokeImg.src = String(data.sprites.front_default);
+      let name = document.createElement("h2");
+      name.innerText = data.name.charAt(0).toUpperCase() + data.name.slice(1);
+      name.style.display = "block";
+      name.style.width = "100%";
+      name.style.height = "35px";
+      name.style.backgroundColor = "darkseagreen";
+      name.style.borderStyle = "solid";
+      name.style.borderWidth = "4px 4px 0px";
+      name.style.textAlign = "center";
+      name.style.padding = "10px";
 
-    div.appendChild(pokeImg);
+      let pokeInfo = document.createElement("div");
+      pokeInfo.setAttribute("id", "pokeinfo");
+      // get the name and some information
+      // bio
+      let pokeBio = document.createElement("div");
+      pokeBio.innerHTML += `<h4>Bio:</h4>`;
+      pokeBio.innerHTML += `<div> Height: ${data.height}</div>`;
+      pokeBio.innerHTML += `<div> Weight: ${data.weight}</div>`;
 
-    document.querySelector("#root").append(div);
-    // console.log(data.sprites.front_default);
+      // ability info
+      let pokeAbilities = document.createElement("div");
+      pokeAbilities.innerHTML += `<h4>Abilities:</h4>`;
+      // append each ability into pokeInfo
+      let abilities = data.abilities;
+      abilities.forEach((e) => {
+        pokeAbilities.innerHTML += `<div>${e.ability.name}</div>`;
+      });
+      // put ability and bio together in pokeinfo
+      pokeInfo.appendChild(pokeBio);
+      pokeInfo.appendChild(pokeAbilities);
+
+      // appending
+      pokeviewport.appendChild(pokeImg);
+      pokeviewport.appendChild(name);
+      pokeviewport.appendChild(pokeInfo);
+    } else {
+      pokeviewport = document.getElementById("pokemon-viewport");
+      pokeviewport.innerHTML = `<h3>Pokemon doesn't exist</h3>`;
+    }
   };
   // send the request
   request.send();
